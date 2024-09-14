644124569d6b999a9a0af48610185c85b0278a07
Gayani Dewindi Vitharana <gayanivitharana0612@gmail.com>
2024-09-14 14:11:22 +0530

    config

--- test.sh
+++
@@ -1,6 +0,0 @@
-response=$(curl -s http://localhost:30001)
-if [[ "$response" == *"Hello from the Coinbase service!"* ]]; then
-  echo "Green Deployment Passed!"
-else
-  echo "Green Deployment Failed!" && exit 1
-fi
